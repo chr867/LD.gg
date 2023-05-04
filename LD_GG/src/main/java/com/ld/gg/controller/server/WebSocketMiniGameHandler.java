3c52894364817d35f6b6b87fc6a9f96d3fbd3e8f
chr867 <124451478+chr867@users.noreply.github.com>
2023-05-04 13:58:27 +0900

    Refactor : WebSocketSession 저장 방식 변경
    Feat : 새로운 기능 추가
    Fix : 버그 수정 또는 typo
    Refactor : 리팩토링
    Design : CSS 등 사용자 UI 디자인 변경
    Comment : 필요한 주석 추가 및 변경
    Style : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    Test : 테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    Chore : 위에 걸리지 않는 기타 변경사항(빌드 스크립트 수정, assets image, 패키지 매니저 등)
    Init : 프로젝트 초기 생성
    Rename : 파일 혹은 폴더명 수정하거나 옮기는 경우
    Remove : 파일을 삭제하는 작업만 수행하는 경우

--- LD_GG/src/main/java/com/ld/gg/controller/server/WebSocketMiniGameHandler.java
+++ LD_GG/src/main/java/com/ld/gg/controller/server/WebSocketMiniGameHandler.java
@@ -5,7 +5,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
@@ -19,15 +21,23 @@ import com.ld.gg.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class WebSocketHandler extends TextWebSocketHandler{
+public class WebSocketMiniGameHandler extends TextWebSocketHandler{
 	@Autowired
 	ChatService cs;
-	List<WebSocketSession> session_list = new ArrayList<>();
+	Set<WebSocketSession> session_set = new HashSet<>();
+	Set<String> email_set= new HashSet<>();
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		session_list.add(session);
-		log.info("session = {}", session.getAttributes());
+		String email = (String) session.getAttributes().get("email");
+
+		if(!email_set.contains(email)) {
+			session_set.add(session);
+			email_set.add(email);
+		}
+		
+
+		log.info("session_len = {}", session_set.size());
 	}
 	
 	@Override
@@ -41,7 +51,7 @@ public class WebSocketHandler extends TextWebSocketHandler{
 		cm.setMini_game_chat_time(Timestamp.valueOf(LocalDateTime.now(ZoneId.of("Asia/Seoul"))));
 		cs.save_minigame_chat(cm);
 
-		for (WebSocketSession s : session_list) {
+		for (WebSocketSession s : session_set) {
 			if(s == session) {
 				continue;
 			}
@@ -52,6 +62,8 @@ public class WebSocketHandler extends TextWebSocketHandler{
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		session_list.remove(session);
+		session_set.remove(session);
+		email_set.remove(session.getAttributes().get("email"));
+
 	}
 }
