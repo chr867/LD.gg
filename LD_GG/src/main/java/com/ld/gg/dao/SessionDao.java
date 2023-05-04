5c101d521ee826fdcac96489a37f2044a8be6edd
taehyun <toss7185@gmail.com>
2023-05-04 11:52:05 +0900

    feat: 관리자 기능 추가

---
+++ LD_GG/src/main/java/com/ld/gg/dao/SessionDao.java
@@ -0,0 +1,9 @@
+package com.ld.gg.dao;
+
+import com.ld.gg.dto.SessionDto;
+
+public interface SessionDao {
+
+	boolean insertSession(SessionDto sDto);
+
+}
--- LD_GG/src/main/java/com/ld/gg/userClass/SessionInterceptor.java
+++ LD_GG/src/main/java/com/ld/gg/userClass/SessionInterceptor.java
@@ -2,10 +2,20 @@ package com.ld.gg.userClass;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ld.gg.dao.SessionDao;
+import com.ld.gg.dto.SessionDto;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class SessionInterceptor implements HandlerInterceptor {
+	
+	@Autowired
+	SessionDao sDao;
 	
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -14,21 +24,35 @@ public class SessionInterceptor implements HandlerInterceptor {
             response.sendRedirect("/"); // 홈페이지로 이동
             return false;
         }else {
-        	return true; // 로그인한 경우
+        	String email = (String) request.getSession().getAttribute("email");
+            String ipAddress = request.getRemoteAddr();
+            String requestURI = request.getRequestURI();
+            String httpMethod = request.getMethod();
+            String userAgent = request.getHeader("User-Agent");
+            log.info("email" + email);
+            log.info("ipAddress"+ipAddress);
+            log.info("requestURI"+requestURI);
+            log.info("httpMethod"+httpMethod);
+            log.info("userAgent"+userAgent);
+            
+            SessionDto sDto = new SessionDto();
+            sDto.setEmail(email);
+            sDto.setIpAddress(ipAddress);
+            sDto.setRequestURI(requestURI);
+            sDto.setHttpMethod(httpMethod);
+            sDto.setUserAgent(userAgent);
+            boolean insertResult = sDao.insertSession(sDto);
+        	return insertResult; 
         }
     }
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 }
