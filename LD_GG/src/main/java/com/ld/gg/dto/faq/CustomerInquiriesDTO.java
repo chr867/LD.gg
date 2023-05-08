82c17bc91473010bfb3aaedfe7118b424c607064
bluetype55 <bluetype55@naver.com>
2023-05-08 16:56:03 +0900

    fix : 맞춤멘토 버그 수정 및 faq dto 추가

--- LD_GG/src/main/java/com/ld/gg/dto/faq/CustomerInquiriesDTO.java
+++ LD_GG/src/main/java/com/ld/gg/dto/faq/CustomerInquiriesDTO.java
@@ -1,5 +1,20 @@
 package com.ld.gg.dto.faq;
 
-public class CustomerInquiriesDTO {
+import org.apache.ibatis.type.Alias;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain=true)
+@Alias("ci")
+public class CustomerInquiriesDTO {
+	private int inquiries_id;
+	private String inquiries_title;
+	private String inquiries_info;
+	private String costomer_email;
 }
--- LD_GG/src/main/java/com/ld/gg/dto/faq/CustomerServiceDTO.java
+++ LD_GG/src/main/java/com/ld/gg/dto/faq/CustomerServiceDTO.java
@@ -1,5 +1,20 @@
 package com.ld.gg.dto.faq;
 
-public class CustomerServiceDTO {
+import org.apache.ibatis.type.Alias;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain=true)
+@Alias("cs")
+public class CustomerServiceDTO {
+	private int cs_id;
+	private String cs_title;
+	private String cs_info;
+	private String cs_answerer_email;
 }
