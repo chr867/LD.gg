d0c48836812c0b6e2b4ca1f063d06e3d35dba913
chr867 <124451478+chr867@users.noreply.github.com>
2023-05-08 15:21:14 +0900

    feat : summoner_info 테이블 데이터 삽입
    Feat : 새로운 기능 추가
    Fix : 버그 수정 또는 typo
    Refactor : 리팩토링
    Design : CSS 등 사용자 UI 디자인 변경
    Comment : 필요한 주석 추가 및 변경
    Style : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    Test : 테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    Chore : 위에 걸리지 않는 기타 변경사항(빌드 스크립트 수정, assets image, 패키지 매니저 등)
    Init : 프로젝트 초기 생성
    Rename : 파일 혹은 폴더명 수정하거나 옮기는 경우
    Remove : 파일을 삭제하는 작업만 수행하는 경우

--- python/summoner/summoner.py
+++ python/summoner/summoner.py
@@ -7,13 +7,14 @@ import my_utils as mu
 import json
 import multiprocessing as mp
 import logging
+import private
 tqdm.pandas()
-
-api_key = mu.riot_api_key
 
 tiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND']
 divisions = ['IV', 'III', 'II', 'I']
+api_keys = private.api_keys
 
+api_it = iter(api_keys)
 summoner_leagues = []
 for tier in tqdm(tiers):
     for division in tqdm(divisions):
@@ -22,13 +23,17 @@ for tier in tqdm(tiers):
         page_p = 1
         while True:
             try:
+                api_key = next(api_it)
+            except StopIteration:
+                api_it = iter(api_keys)
+                api_key = next(api_it)
+
+            try:
                 url = f'https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page={page_p}&api_key={api_key}'
                 res_p = requests.get(url).json()
                 tmp_lst.append(res_p[0]['summonerId'])
-
             except IndexError:
                 break
-
             except Exception as e:
                 print(f'suummoner names {e} 예외 발생 {res_p}, {api_key}')
                 time.sleep(10)
@@ -64,11 +69,54 @@ rank_result_df.progress_apply(lambda x: insert(x, sql_conn), axis=1)
 sql_conn.commit()
 sql_conn.close()
 
-# summoner_info_columns = ['summoner_name', 's_level', 'profile_icon_id', 'games', 'tier', 'wins', 'losses', 'lp',
-#                          'ranking', 'revision_date']
-# league = ['tier', 'leagueId', 'queueType', 'summonerName', 'leaguePoints', 'wins', 'losses', 'tier', 'wins'+'loses']
 
-summoner_ids = [i['summonerId'] for i in summoner_leagues]
+summoner_names = [i['summonerName'] for i in summoner_leagues]
+summoner_info = []
+for summoner_name in tqdm(summoner_names):
+    tmp = []
+    try:
+        url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}'
+        res = requests.get(url).json()
+        tmp.append(res['accountId'])
+    except Exception as e:
+        print(f'suummoner names {e} 예외 발생 {res}, {summoner_name}, {api_key}')
+        continue
+    summoner_info.append(res)
 
-for summoner_id in summoner_ids:
-    url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/2-SCJRP1c3zeRE8A_sSDinXqBIo5A53o3mZZDZTxFOiYFWY-6I1IljjVRw?api_key={api_key}'
+summoner_names = [i['summonerName'] for i in summoner_leagues]
+summoner_info = []
+for summoner_name in tqdm(summoner_names):
+    tmp = []
+    try:
+        url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}'
+        res = requests.get(url).json()
+        tmp.append(res['accountId'])
+    except Exception as e:
+        print(f'suummoner names {e} 예외 발생 {res}, {summoner_name}, {api_key}')
+        continue
+    summoner_info.append(res)
+
+info_df = pd.DataFrame(summoner_info)
+info_result_df = info_df[['name', 'summonerLevel', 'profileIconId', 'revisionDate']]
+info_result_df.columns = ['summoner_name', 's_level', 'profile_icon_id', 'revision_date']
+
+for_merge_df = rank_result_df[['summoner_name', 'match_count', 'tier', 'wins', 'losses', 'lp', 'division']][:20]
+for_merge_df.columns = ['summoner_name', 'games', 'tier', 'wins', 'losses', 'lp', 'ranking']
+
+info_merged_df = pd.merge(info_result_df, for_merge_df)
+info_merged_df.keys()
+def info_insert(s, conn):
+    insert_query = (
+        f'INSERT INTO SUMMONER_INFO (SUMMONER_NAME, S_LEVEL, PROFILE_ICON_ID, GAMES, TIER, WINS, LOSSES, LP, RANKING) '
+        f'VALUES ({repr(s.summoner_name)}, {s.s_level}, {s.profile_icon_id}, {s.games}, {repr(s.tier)}, {s.wins}, {s.losses}, {s.lp}, {repr(s.ranking)}) '
+        f'ON DUPLICATE KEY UPDATE '
+        f'SUMMONER_NAME = {repr(s.summoner_name)}, S_LEVEL = {s.s_level}, PROFILE_ICON_ID = {s.profile_icon_id}, GAMES = {s.games}, '
+        f'TIER = {repr(s.tier)}, WINS = {s.wins}, LOSSES = {s.losses}, LP = {s.lp}, RANKING = {repr(s.ranking)}'
+    )
+    mu.mysql_execute_dict(insert_query, conn)
+
+sql_conn = mu.connect_mysql()
+info_merged_df.progress_apply(lambda x: info_insert(x, sql_conn), axis=1)
+sql_conn.commit()
+sql_conn.close()
+
