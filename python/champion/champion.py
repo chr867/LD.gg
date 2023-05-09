04fec20af07f1962b86a0cb7bf377201b5216452
taehyun <toss7185@gmail.com>
2023-05-09 13:49:01 +0900

    feat: 일반 아이템 정제 코드 추가

--- python/champion/champion.py
+++ python/champion/champion.py
@@ -78,48 +78,43 @@ def rune_data(df):
 
 
 rune_data = rune_data(df)
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # 아이템 데이터 정제
-result = []
-for i in tqdm(range(len(df))):
-    raw_data = df.iloc[i]['matches']['info']['participants']
-    for summoner in range(len(raw_data)):
-        lst = []
-        lst.append(raw_data[summoner]['championId'])
-        try:
-            lst.append(raw_data[summoner]['challenges']['mythicItemUsed'])
-        except:
-            lst.append(0)
-        lst.append(raw_data[summoner]['item0'])
-        lst.append(raw_data[summoner]['item1'])
-        lst.append(raw_data[summoner]['item2'])
-        lst.append(raw_data[summoner]['item3'])
-        lst.append(raw_data[summoner]['item4'])
-        lst.append(raw_data[summoner]['item5'])
-        lst.append(raw_data[summoner]['item6'])
-        lst.append(raw_data[summoner]['win'])
-        result.append(lst)
-columns = ['championId', 'mythicItemUsed', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'win']
-item_df = pd.DataFrame(result, columns=columns)
-item_df['win'] = item_df['win'].astype(int)
-# ----------------------------------------------------------------------------------------------------------------------
-item_columns = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5']
-melted_df = item_df.melt(id_vars=['championId'], value_vars=item_columns, var_name='item_col', value_name='itemId')
+def item_df(df):
+    result = []
+    for i in tqdm(range(len(df))):
+        raw_data = df.iloc[i]['matches']['info']['participants']
+        for summoner in range(len(raw_data)):
+            lst = []
+            lst.append(raw_data[summoner]['championId'])
+            try:
+                lst.append(raw_data[summoner]['challenges']['mythicItemUsed'])
+            except:
+                lst.append(0)
+            lst.append(raw_data[summoner]['item0'])
+            lst.append(raw_data[summoner]['item1'])
+            lst.append(raw_data[summoner]['item2'])
+            lst.append(raw_data[summoner]['item3'])
+            lst.append(raw_data[summoner]['item4'])
+            lst.append(raw_data[summoner]['item5'])
+            lst.append(raw_data[summoner]['item6'])
+            lst.append(raw_data[summoner]['win'])
+            result.append(lst)
+    columns = ['championId', 'mythicItemUsed', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'win']
+    item_df = pd.DataFrame(result, columns=columns)
+    item_df['win'] = item_df['win'].astype(int)
 
-non_zero_items = melted_df[melted_df['itemId'] != 0]
-item_frequency = non_zero_items.groupby(['championId', 'itemId']).size().reset_index(name='frequency')
+    return item_df
 
 
-def top_n_items(df, n=10):
-    return df.nlargest(n, 'frequency')
-
-
-top_items_df = item_frequency.groupby('championId', group_keys=False).apply(top_n_items)
+item_df = item_df(df)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 챔피언 신발 데이터
-def shoe_data(item_df):
+def shoes_data(item_df):
     shoe_items = [3111, 3117, 3009, 3047, 3006, 3158, 3020]
     champion_shoe_counts = {}
     champion_win_counts = {}
@@ -160,8 +155,87 @@ def shoe_data(item_df):
     merged_df = top_2_shoes_per_champion.copy()
     merged_df['rank'] = merged_df['rank'].astype(int)
     merged_df = merged_df[['championId', 'itemId', 'usage_count', 'win_count', 'winrate', 'rank']]
+    merged_df = merged_df.fillna(0)
     return merged_df
 
 
-shoe_data = shoe_data(item_df)
+shoes_data = shoes_data(item_df)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 신화 아이템 정제
+def get_top_mythic_items(df):
+    result = []
+    for i in tqdm(range(len(df))):
+        raw_data = df.iloc[i]['matches']['info']['participants']
+        for summoner in range(len(raw_data)):
+            champion_id = raw_data[summoner]['championId']
+            mythic_item_used = raw_data[summoner]['challenges']['mythicItemUsed'] if 'mythicItemUsed' in \
+                                                                                     raw_data[summoner][
+                                                                                         'challenges'] else 0
+            win = raw_data[summoner]['win']
+            result.append([champion_id, mythic_item_used, win])
+
+    item_df = pd.DataFrame(result, columns=['championId', 'mythicItemUsed', 'win'])
+    item_df['mythicItemUsed'] = item_df['mythicItemUsed'].astype(int)
+    item_df['win'] = item_df['win'].astype(int)
+
+    item_df = item_df[item_df['mythicItemUsed'] != 0]
+
+    top_items = []
+    for champion_id, group in item_df.groupby('championId'):
+        top_items_df = group.groupby('mythicItemUsed').agg({'mythicItemUsed': 'count', 'win': 'sum'})
+        top_items_df.columns = ['usage_count', 'win_count']
+        top_items_df['winrate'] = round((top_items_df['win_count'] / top_items_df['usage_count']) * 100, 2)
+        top_items_df = top_items_df.sort_values(['usage_count', 'winrate'], ascending=[False, False])
+        top_items_df = top_items_df.iloc[:3].reset_index()
+        top_items_df['championId'] = champion_id
+        top_items.append(top_items_df)
+
+    top_items_df = pd.concat(top_items)
+    top_items_df['rank'] = top_items_df.groupby('championId')['usage_count'].rank(ascending=False, method='first')
+
+    return top_items_df[['championId', 'mythicItemUsed', 'usage_count', 'win_count', 'winrate', 'rank']]
+
+
+mysitic_item_data = get_top_mythic_items(df)
+# ----------------------------------------------------------------------------------------------------------------------
+# 아이템 정제 (신화, 신발 제외)
+mysitic_item_lst = mysitic_item_data['mythicItemUsed'].unique()
+shoes_lst = [3111, 3117, 3009, 3047, 3006, 3158, 3020]
+
+
+def top_item_data(item_df):
+    item_columns = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5']
+    melted_df = item_df.melt(id_vars=['championId', 'win'], value_vars=item_columns, var_name='item_col',
+                             value_name='itemId')
+
+    non_zero_items = melted_df[melted_df['itemId'] != 0]
+    item_frequency = non_zero_items.groupby(['championId', 'itemId']).size().reset_index(name='frequency')
+    # 승리한 경우의 아이템 사용 횟수 집계
+    non_zero_items_wins = non_zero_items[non_zero_items['win'] == 1]
+    item_wins = non_zero_items_wins.groupby(['championId', 'itemId']).size().reset_index(name='wins')
+
+    # item_frequency에 승리 횟수를 추가
+    item_frequency = item_frequency.merge(item_wins, on=['championId', 'itemId'], how='left')
+    item_frequency['wins'].fillna(0, inplace=True)
+
+    def top_n_items(df, excluded_items, n=3):
+        df_filtered = df[~df['itemId'].isin(excluded_items)]
+        return df_filtered.nlargest(n, 'frequency')
+
+    # 제외할 아이템 목록 정의
+    mysitic_item_lst = mysitic_item_data['mythicItemUsed'].unique()
+    shoes_lst = [3111, 3117, 3009, 3047, 3006, 3158, 3020]
+    excluded_items = list(mysitic_item_lst) + shoes_lst
+
+    # 제외할 아이템 목록을 전달하여 상위 10개의 아이템을 추출
+    top_items_df = item_frequency.groupby('championId', group_keys=False).apply(
+        lambda x: top_n_items(x, excluded_items))
+    top_items_df['wins'] = top_items_df['wins'].astype(int)
+    top_items_df['winrate'] = round((top_items_df['wins'] / top_items_df['frequency']) * 100, 2)
+    return top_items_df
+
+
+top_item_data = top_item_data(item_df)
 # ----------------------------------------------------------------------------------------------------------------------
