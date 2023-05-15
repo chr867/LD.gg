4696573b8759a236a5d9507444394c5a09346f44
chr867 <124451478+chr867@users.noreply.github.com>
2023-05-15 14:57:49 +0900

    Feat : record 테이블 insert 구현
    Feat : 새로운 기능 추가
    Fix : 버그 수정 또는 typo
    Refactor : 리팩토링
    Design : CSS 등 사용자 UI 디자인 변경
    Comment : 필요한 주석 추가 및 변경
    Style : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    Test : 테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    Chore : 위에 걸리지 않는 기타 변경사항(빌드 스크립트 수정, assets image, 패키지 매니저 등)
    Init : 프로젝트 초기 생성
    Rename : 파일 혹은 폴더명 수정하거나 옮기는 경우
    Remove : 파일을 삭제하는 작업만 수행하는 경우

---
+++ python/match/record.py
@@ -0,0 +1,112 @@
+import datetime
+import time
+import pandas as pd
+import requests
+from tqdm import tqdm
+import data_load
+import my_utils as mu
+import json
+import logging
+tqdm.pandas()
+
+sql_conn = mu.connect_mysql()
+df = data_load.raw_matches_timeline_data(0)
+sql_conn.close()
+
+df['matches'] = df.apply(lambda x: json.loads(x['matches']), axis=1)
+df['timeline'] = df.apply(lambda x: json.loads(x['timeline']), axis=1)
+
+record_df_creater = []
+for idx, i in enumerate(df['matches']):
+    for p_idx, p in enumerate(i['participants']):
+        if p_idx <= 4:
+            versus_index = 6
+            deaths = i['participants'][versus_index]['objectives'][0]['champion']['kills']
+        else:
+            versus_index = 0
+            deaths = i['participants'][versus_index]['objectives'][0]['champion']['kills']
+
+        record_df_creater.append([
+            df.iloc[idx]['match_id'],
+            p['summonerName'],
+            i['gameDuration'],
+            i['gameMode'],
+            p['championName'],
+            p['champ_level'],
+            p['teamId'],
+            p['win'],
+            p['kills'],
+            p['deaths'],
+            p['assists'],
+            p['kda'],
+            # cs
+            (df.iloc[idx]['timeline'][str(len(df.iloc[idx]['timeline']) - 1)]['participantFrames'][p_idx]['minionsKilled'] +
+            df.iloc[idx]['timeline'][str(len(df.iloc[idx]['timeline']) - 1)]['participantFrames'][p_idx]['jungleMinionsKilled']),
+            p['style0'],
+            p['perks0'][0],
+            p['perks0'][1],
+            p['perks0'][2],
+            p['perks0'][3],
+            p['style1'],
+            p['perks1'][0],
+            p['perks1'][1],
+            # rune_stat1
+            p['offense'],
+            # rune_stat2
+            p['flex'],
+            # rune_stat3
+            p['defense'],
+            p['item0'],
+            p['item1'],
+            p['item2'],
+            p['item3'],
+            p['item4'],
+            p['item5'],
+            p['item6'],
+            p['red_ward_placed'],
+            p['total_damage_dealt'],
+            p['totalDamageDealtToChampions'],
+            p['totalDamageTaken'],
+            p['teamPosition'],
+            p['summoner1Id'],
+            p['summoner2Id'],
+            p['objectives'][0]['baron']['kills'],
+            p['objectives'][0]['champion']['kills'],
+            deaths,
+            p['objectives'][0]['dragon']['kills'],
+            p['objectives'][0]['tower']['kills'],
+            p['sight_point']
+        ])
+
+columns = ['match_id', 'summoner_name','game_duration', 'game_mode', 'champ_name', 'champ_level',
+           'team_id', 'win', 'kills', 'deaths', 'assists', 'kda', 'cs', 'main_rune', 'main_rune1', 'main_rune2',
+           'main_rune3', 'main_rune4', 'sub_rune', 'sub_rune1', 'sub_rune2', 'rune_stat1', 'rune_stat2', 'rune_stat3',
+           'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7',
+           'red_ward_placed', 'total_damage_dealt', 'dealt_to_champ', 'damage_taken', 'lane', 'spell1', 'spell2',
+           'team_baron_kills', 'team_champion_kills', 'team_champion_deaths', 'team_dragon_kills', 'team_tower_kills',
+           'sight_point']
+
+record_df = pd.DataFrame(record_df_creater, columns=columns)
+
+def insert(t, conn):
+    sql = (
+        f'insert ignore into record (match_id, summoner_name, game_duration, game_mode, champ_name, champ_level,'
+        f'team_id, win, kills, deaths, assists, kda, cs, main_rune, main_rune1, main_rune2, main_rune3, main_rune4,'
+        f'sub_rune, sub_rune1, sub_rune2, rune_stat1, rune_stat2, rune_stat3, item1, item2, item3, item4, item5, item6,'
+        f'item7, red_ward_placed, total_damage_dealt, dealt_to_champ, damage_taken, lane, spell1, spell2, team_baron_kills,'
+        f'team_champion_kills, team_champion_deaths, team_dragon_kills, team_tower_kills, sight_point) '
+        f'values ({repr(t.match_id)}, {repr(t.summoner_name)}, {t.game_duration}, {repr(t.game_mode)}, {repr(t.champ_name)},'
+        f'{t.champ_level}, {t.team_id}, {t.win}, {t.kills}, {t.deaths}, {t.assists}, {t.kda}, {t.cs}, {t.main_rune}, '
+        f'{t.main_rune1}, {t.main_rune2}, {t.main_rune3}, {t.main_rune4}, {t.sub_rune}, {t.sub_rune1}, {t.sub_rune2}, '
+        f'{t.rune_stat1}, {t.rune_stat2}, {t.rune_stat3}, {t.item1}, {t.item2}, {t.item3}, {t.item4}, '
+        f'{t.item5}, {t.item6}, {t.item7}, {t.red_ward_placed}, {t.total_damage_dealt}, {t.dealt_to_champ}, {t.damage_taken}, '
+        f'{repr(t.lane)}, {t.spell1}, {t.spell2}, {t.team_baron_kills}, {t.team_champion_kills}, {t.team_champion_deaths},'
+        f' {t.team_dragon_kills}, {t.team_tower_kills}, {t.sight_point})'
+    )
+    mu.mysql_execute(sql, conn)
+
+sql_conn = mu.connect_mysql()
+record_df.progress_apply(lambda x: insert(x, sql_conn), axis=1)
+sql_conn.commit()
+sql_conn.close()
+
